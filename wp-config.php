8d99e7ebb167d5f50e094bcd5411db91f8f35baf
kakadacancer-code <your@email.com>
2026-07-14 13:23:07 +0700

    already commit

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'vbanx_db' );
+define( 'DB_NAME', 'vbanx_project' );
 
 /** Database username */
 define( 'DB_USER', 'root' );
